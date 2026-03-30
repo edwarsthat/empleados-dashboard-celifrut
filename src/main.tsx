548351345840ca4f8f1861ecdb7a39b36488f71e
Jincoxx <carnagechu@gmail.com>
2026-03-30 15:20:44 -0500

    por si las moscas copia

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Login from './pages/Login.tsx'
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/verify" replace />} />
         <Route path="/verify" element={<App />} />
         <Route path="/login"  element={<Login />} />
       </Routes>
